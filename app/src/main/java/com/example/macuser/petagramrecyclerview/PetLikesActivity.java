aa6017174a49a072cc39f4d6adcad3beeee8f5cf
Hugo Reyes <hugo.reyesamell@gmail.com>
2017-11-29 22:18:58 -0400

    there is a problem with extra intent data

--- app/src/main/java/com/example/macuser/petagramrecyclerview/PetLikesActivity.java
+++ app/src/main/java/com/example/macuser/petagramrecyclerview/PetLikesActivity.java
@@ -8,6 +8,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 
+import java.util.ArrayList;
+
 public class PetLikesActivity extends AppCompatActivity {
 
     private Pets petsArrayList;
@@ -32,7 +34,9 @@ public class PetLikesActivity extends AppCompatActivity {
             // Parse the string to a User object
             String objAsJson = bundle.getString("pets");
 
-            this.petsArrayList.setPets(Pets.fromJson(objAsJson).getPets());
+            ArrayList<Pet> test = Pets.fromJson(objAsJson).getPets();
+            System.out.println("test");
+            //this.petsArrayList.setPets(Pets.fromJson(objAsJson).getPets());
         }
 
     }

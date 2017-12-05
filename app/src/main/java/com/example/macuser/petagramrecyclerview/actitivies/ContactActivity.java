b98e631f96cfc6313c0098097085f85f87cb7174
Hugo Reyes <hugo.reyesamell@gmail.com>
2017-12-04 23:53:29 -0400

    started to work with email

--- app/src/main/java/com/example/macuser/petagramrecyclerview/actitivies/ContactActivity.java
+++ app/src/main/java/com/example/macuser/petagramrecyclerview/actitivies/ContactActivity.java
@@ -1,5 +1,6 @@
 package com.example.macuser.petagramrecyclerview.actitivies;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
@@ -9,6 +10,10 @@ import android.widget.Button;
 import com.example.macuser.petagramrecyclerview.R;
 
 import java.util.Properties;
+
+import javax.mail.Authenticator;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
 
 /**
  * Created by macuser on 12/3/17.
@@ -40,11 +45,30 @@ public class ContactActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
-                //Properties properties = new Properties();
-                //properties.put()
+//                Properties properties = new Properties();
+//                properties.put("mail.smtp.host", "smtp.gmail.com");
+//                properties.put("mail.smtp.socketFactory.port", "465");
+//                properties.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+//                properties.put("mail.smtp.auth", "true");
+//                properties.put("mail.smtp.port", "465");
+//
+//                session = Session.getDefaultInstance(properties, new Authenticator() {
+//                    @Override
+//                    protected PasswordAuthentication getPasswordAuthentication() {
+//                        String email = "p&e&t&a&g&r&a&m&t&e&s&t&@&g&m&a&i&l&.&co&m";
+//                        String clave = "p&e&t&a&2&2&3&4&%&t&7&$";
+//                        return new PasswordAuthentication(email.replace("&", ""), clave.replace("&", ""))
+//                    }
+//                });
+//
+//                pdialog = ProgressDialog.show(context, "", "Sending Mail...", true);
 
             }
         });
+
+        // correo
+        // p&e&t&a&g&r&a&m&t&e&s&t&@&g&m&a&i&l&.&co&m
+        // p&e&t&a&2&2&3&4&%&t&7&$
 
     }
 
